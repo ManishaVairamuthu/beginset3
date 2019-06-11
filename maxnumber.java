3a03a88e96ab34e51f00f68b6b699e8d97c95b62
Manisha V <manisha971026@gmail.com>
2019-06-11 14:38:36 +0530

    Create maxnumber.java

---
+++ maxnumber.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+import java.lang.*;
+
+
+
+class minnumber
+
+{  
+
+    public static void main(String args[])
+
+    {
+
+        
+
+     Scanner sc=new Scanner(System.in);
+
+        int a=sc.nextInt();
+
+        int b[]=new int[a];
+
+        for(int i=0;i<a;i++)
+
+        {
+
+            b[i]=sc.nextInt();
+
+        }
+
+        int m=b[1];
+
+        for(int j=0;j<a;j++)
+
+        {
+
+            if(m<b[j])
+
+                m=b[j];
+
+        }
+
+        System.out.print(m);
+
+    }
+
+}
